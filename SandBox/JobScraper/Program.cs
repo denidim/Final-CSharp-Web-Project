cbf403f7103fb873790453b32292a2103f37c0c1
denidim <fs900220@gmail.com>
2022-11-20 23:11:43 +0200

    added Sandbox

--- SandBox/JobScraper/Program.cs
+++ SandBox/JobScraper/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace JobScraper
+﻿using AngleSharp;
+using AngleSharp.Html.Dom;
+using System.Text;
+
+namespace JobScraper
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var url = @"https://www.1001recepti.com/recipes/";
+
+            var config = Configuration.Default.WithDefaultLoader();
+
+            var doc = await BrowsingContext.New(config).OpenAsync(url);
+
+            var categoryName = doc.QuerySelectorAll("#cont_recipe_browse_big > div > a").OfType<IHtmlAnchorElement>().ToList();
+
         }
     }
 }
