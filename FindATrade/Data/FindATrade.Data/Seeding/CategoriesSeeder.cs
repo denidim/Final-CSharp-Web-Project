46ef961f94b4d6900eb10a5e2dcfd8c6715ab0a8
denidim <fs900220@gmail.com>
2022-11-21 12:21:26 +0200

    added categoris seeder

---
+++ FindATrade/Data/FindATrade.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,23 @@
+﻿namespace FindATrade.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            await dbContext.Categories.AddAsync(new Models.Category { Name = "Бояджия" });
+            await dbContext.Categories.AddAsync(new Models.Category { Name = "Програмист" });
+            await dbContext.Categories.AddAsync(new Models.Category { Name = "Графичен Дизайн" });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
