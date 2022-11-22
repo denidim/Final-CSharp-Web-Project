bbbec0476b7bb2ec095e8f4f80b9c2a6cab7f3c0
denidim <fs900220@gmail.com>
2022-11-22 15:08:28 +0200

    changed address to user relation from one to meny to one to one

---
+++ FindATrade/Data/FindATrade.Data/Migrations/20221122115852_ChangedAddressUserRelationToOneToOne.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindATrade.Data.Migrations
+{
+    public partial class ChangedAddressUserRelationToOneToOne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies",
+                column: "AddressId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Companies",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId");
+        }
+    }
+}
