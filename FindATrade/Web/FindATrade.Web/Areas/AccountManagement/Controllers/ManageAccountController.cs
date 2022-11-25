a2394488d07bac406f5fbd11051bc64d76ef661c
denidim <fs900220@gmail.com>
2022-11-25 12:26:08 +0200

    added Account Management Area

---
+++ FindATrade/Web/FindATrade.Web/Areas/AccountManagement/Controllers/ManageAccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindATrade.Web.Areas.AccountManagement.Controllers
+{
+    [Area("AccountManagement")]
+    public class ManageAccountController : Controller
+    {
+        public IActionResult Manage()
+        {
+            // TODO: Get Account Service
+            // TODO: Return Account For Edit
+            return this.View();
+        }
+
+        // TODO Edit Account Action
+    }
+}
