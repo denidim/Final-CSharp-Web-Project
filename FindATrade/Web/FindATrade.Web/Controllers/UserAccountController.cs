7f829cf0c982fa9d7bad0a3720e11d8583657e6f
denidim <fs900220@gmail.com>
2022-11-25 13:05:20 +0200

    added PaidPackages Area added UserAccountController

---
+++ FindATrade/Web/FindATrade.Web/Controllers/UserAccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FindATrade.Web.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        public IActionResult GetAccount()
+        {
+            // TODO: Get account service
+            // TODO: Return account viewmodel
+            return this.View();
+        }
+    }
+}
