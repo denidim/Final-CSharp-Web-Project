a37aae4c26c8b4479c23d36154376df7e8a2fe76
denidim <fs900220@gmail.com>
2022-12-05 01:37:28 +0200

    edited skillModel edited EditCompanyViewModel

--- FindATrade/Web/FindATrade.Web.ViewModels/Company/EditCompanyViewModel.cs
+++ FindATrade/Web/FindATrade.Web.ViewModels/Company/EditCompanyViewModel.cs
@@ -1,15 +1,14 @@
-﻿using AutoMapper;
-using FindATrade.Services.Mapping;
-
-namespace FindATrade.Web.ViewModels.Company
+﻿namespace FindATrade.Web.ViewModels.Company
 {
+    using AutoMapper;
+    using FindATrade.Services.Mapping;
+
     public class EditCompanyViewModel : CreateCompanyInputModel, IMapFrom<Data.Models.Company>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
         public void CreateMappings(IProfileExpression configuration)
         {
-
             configuration.CreateMap<Data.Models.Company, EditCompanyViewModel>()
                 .ForMember(x => x.Image, opt => opt.Ignore());
         }
--- FindATrade/Web/FindATrade.Web.ViewModels/Company/SkillModel.cs
+++ FindATrade/Web/FindATrade.Web.ViewModels/Company/SkillModel.cs
@@ -1,10 +1,13 @@
 ﻿namespace FindATrade.Web.ViewModels.Company
 {
+    using System.ComponentModel.DataAnnotations;
+
     using FindATrade.Data.Models;
     using FindATrade.Services.Mapping;
 
     public class SkillModel : IMapFrom<Skill>
     {
+        [StringLength(30)]
         public string Name { get; set; }
     }
 }
