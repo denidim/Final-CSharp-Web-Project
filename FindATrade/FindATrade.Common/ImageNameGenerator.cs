e405301eb00788f161bd47bbcde9b836257a4307
denidim <fs900220@gmail.com>
2022-12-05 04:39:18 +0200

    added statc ImageNameGenerator

---
+++ FindATrade/FindATrade.Common/ImageNameGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace FindATrade.Common
+{
+    using System;
+    using System.IO;
+
+    public static class ImageNameGenerator
+    {
+        public static string GenerateFileName(string fileName)
+        {
+            var name = Path.GetFileNameWithoutExtension(fileName);
+            var extension = Path.GetExtension(fileName);
+            return $"{name}-{DateTime.Now.ToUniversalTime().ToString("yyyyMMddHHmmss")}{extension}";
+        }
+    }
+}
