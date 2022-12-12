dac009f343b6aadff071f9e6f36eb8c2a447161c
denidim <fs900220@gmail.com>
2022-12-12 04:49:32 +0200

    CompanyAddressId made nullable

--- FindATrade/Data/FindATrade.Data.Models/Company.cs
+++ FindATrade/Data/FindATrade.Data.Models/Company.cs
@@ -39,8 +39,7 @@
 
         public Image Image { get; set; }
 
-        [Required]
-        public int AddressId { get; set; }
+        public int? AddressId { get; set; }
 
         public Address Address { get; set; }
 
---
+++ FindATrade/Data/FindATrade.Data/Migrations/20221212022928_AddressIdMadeNullable.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindATrade.Data.Migrations
+{
+    public partial class AddressIdMadeNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Address_AddressId",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Companies",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies",
+                column: "AddressId",
+                unique: true,
+                filter: "[AddressId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Address_AddressId",
+                table: "Companies",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Address_AddressId",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Companies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_AddressId",
+                table: "Companies",
+                column: "AddressId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Address_AddressId",
+                table: "Companies",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
