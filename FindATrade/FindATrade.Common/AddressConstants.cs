25f18b72b31ad623e474036733bd1c0ed80302f1
denidim <fs900220@gmail.com>
2022-12-13 21:43:48 +0200

    added adress constants

--- FindATrade/FindATrade.Common/AddressConstants.cs
+++ FindATrade/FindATrade.Common/AddressConstants.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FindATrade.Common
+﻿namespace FindATrade.Common
 {
-    internal class AddressConstants
+    public static class AddressConstants
     {
+        public const int StreetMin = 2;
+
+        public const int StreetMax = 50;
+
+        public const string StreetMessage = "{0} must be between {2} and {1}";
+
+        public const int HouseMin = 0;
+
+        public const int HouseMax = 1000;
+
+        public const string HouseName = "House/Flat Number";
+
+        public const string HouseMessage = "{0} must be between {1} and {2}";
+
+        public const int HouseAdditionMin = 3;
+
+        public const int HouseAdditionMax = 50;
+
+        public const string HouseAdditinName = "Addition (optional)";
+
+        public const string HouseAdditionMessage = "{0} must be between {2} and {1}";
+
+        public const int CityMin = 3;
+
+        public const int CityMax = 50;
+
+        public const string CityMessage = "{0} must be between {2} and {1}";
+
+        public const int CountryMin = 3;
+
+        public const int CountryMax = 50;
+
+        public const string CountryMessage = "{0} must be between {2} and {1}";
+
+        public const string PostalCodeName = "Postal Code (optional)";
     }
 }
