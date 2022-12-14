511cc3ec96e2530bdac4b59e4fc145637284ed8d
denidim <fs900220@gmail.com>
2022-12-14 02:10:06 +0200

    added image constant

---
+++ FindATrade/FindATrade.Common/ImageConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace FindATrade.Common
+{
+    public static class ImageConstants
+    {
+        public const string DefaultImage = "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+    }
+}
