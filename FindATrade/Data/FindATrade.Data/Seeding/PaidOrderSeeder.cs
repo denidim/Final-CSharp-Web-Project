5d363a226fd94a2f1f17c7e87c868bea268c3ade
denidim <fs900220@gmail.com>
2022-12-14 08:07:51 +0200

    added PaidOrderPackageTypeSeed

---
+++ FindATrade/Data/FindATrade.Data/Seeding/PaidOrderSeeder.cs
@@ -0,0 +1,21 @@
+﻿namespace FindATrade.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class PaidOrderSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.PaidOrders.Any())
+            {
+                return;
+            }
+
+            await dbContext.PaidOrderPackageTypes.AddAsync(new Models.PaidOrderPackageType { Name = "Regular Subscription", Terms = "Your service will be shown on the first pages for 30 days", Price = 10.00m });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
